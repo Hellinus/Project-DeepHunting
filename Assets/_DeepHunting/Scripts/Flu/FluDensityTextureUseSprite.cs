ab8a2aded14bc011e8b57d1017bad239e0d7d6ab
Hellinus <hellinus@outlook.com>
2024-06-10 01:59:27 +0800

    yellow room

---
+++ Assets/_DeepHunting/Scripts/Flu/FluDensityTextureUseSprite.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Fluxy;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class FluDensityTextureUseSprite : MonoBehaviour
+{ 
+    public SpriteRenderer CharacerSpriteRender;
+    protected FluxyTarget _fluTarget;
+
+    protected Sprite _sprite;
+    protected Rect _rect;
+    protected Texture2D _texture;
+
+    private void Start()
+    {
+        _fluTarget = GetComponent<FluxyTarget>();
+        
+        _sprite = CharacerSpriteRender.sprite;
+        
+        _rect = _sprite.rect;
+        _texture = new Texture2D((int)_rect.width, (int)_rect.height);
+ 
+        _texture.SetPixels(0, 0, (int)_rect.width, (int)_rect.height, _sprite.texture.GetPixels((int)_rect.x, (int)_rect.y, (int)_rect.width, (int)_rect.height));
+        _texture.Apply();
+        
+        _fluTarget.densityTexture = _texture;
+
+    }
+}
